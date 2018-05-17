4c42985a95f73ee26dbdb66a60180ed9de9b6d0d
David M <maniac@case-of.org>
2018-05-17 15:00:06 +0200

    Added MacOS support to UnixSocket libary

--- src/main/java/cx/ath/matthew/unix/UnixServerSocket.java
+++ src/main/java/cx/ath/matthew/unix/UnixServerSocket.java
@@ -28,17 +28,28 @@ package cx.ath.matthew.unix;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import com.github.hypfvieh.system.NativeLibraryLoader;
+import com.github.hypfvieh.util.SystemUtil;
 
 /**
  * Represents a listening UNIX Socket.
  */
 public class UnixServerSocket implements Closeable {
     static {
-        //System.loadLibrary("unix-java");
-        NativeLibraryLoader.loadLibrary(true, "libunix-java.so", "lib/");
+        if (SystemUtil.isMacOs()) {
+            String macOsMajorVersion = SystemUtil.getMacOsMajorVersion();
+            List<String> osVersionsWithSameLib = Arrays.asList("10.6", "10.7", "10.8", "10.9", "10.10", "10.11", "10.12", "10.13");
+            if (osVersionsWithSameLib.contains(macOsMajorVersion)) {
+                NativeLibraryLoader.loadLibrary(true, "libunix-java.so", "macos/" + macOsMajorVersion + "/");
+            }
+        } else {
+            NativeLibraryLoader.loadLibrary(true, "libunix-java.so", "lib/");
+        }
     }
+
 
     private UnixSocketAddress address = null;
     private boolean           bound   = false;
--- src/main/java/cx/ath/matthew/unix/UnixSocket.java
+++ src/main/java/cx/ath/matthew/unix/UnixSocket.java
@@ -31,11 +31,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.hypfvieh.system.NativeLibraryLoader;
+import com.github.hypfvieh.util.SystemUtil;
 
 /**
  * Represents a UnixSocket.
@@ -43,8 +46,17 @@ import com.github.hypfvieh.system.NativeLibraryLoader;
 public class UnixSocket extends Socket implements Closeable {
 
     static {
-        NativeLibraryLoader.loadLibrary(true, "libunix-java.so", "lib/");
+        if (SystemUtil.isMacOs()) {
+            String macOsMajorVersion = SystemUtil.getMacOsMajorVersion();
+            List<String> osVersionsWithSameLib = Arrays.asList("10.6", "10.7", "10.8", "10.9", "10.10", "10.11", "10.12", "10.13");
+            if (osVersionsWithSameLib.contains(macOsMajorVersion)) {
+                NativeLibraryLoader.loadLibrary(true, "libunix-java.so", "macos/" + macOsMajorVersion + "/");
+            }
+        } else {
+            NativeLibraryLoader.loadLibrary(true, "libunix-java.so", "lib/");
+        }
     }
+
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
