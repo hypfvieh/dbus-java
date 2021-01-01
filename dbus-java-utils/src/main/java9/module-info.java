8bc6ae08921b38326810bc57983cbe6f6a8168d1
Brett Smith <tanktarta@gmail.com>
2021-01-01 22:37:29 +0000

    Return to SLF4J 1.7.30  (it has an Automatic-Module-Name), replace tabs in source with spaces.

--- dbus-java-utils/src/main/java9/module-info.java
+++ dbus-java-utils/src/main/java9/module-info.java
@@ -1,8 +1,8 @@
 module org.freedesktop.dbus.utils {
-	exports org.freedesktop.dbus.utils.bin;
-	exports org.freedesktop.dbus.utils.generator;
-	exports org.freedesktop.dbus.viewer;
-	requires transitive org.freedesktop.dbus;
-	requires java.xml;
-	requires java.desktop;
+    exports org.freedesktop.dbus.utils.bin;
+    exports org.freedesktop.dbus.utils.generator;
+    exports org.freedesktop.dbus.viewer;
+    requires transitive org.freedesktop.dbus;
+    requires java.xml;
+    requires java.desktop;
 }
