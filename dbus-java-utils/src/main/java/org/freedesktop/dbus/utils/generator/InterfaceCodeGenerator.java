40fa077c6d5bc668158ba9f5696ba4b2d951d86a
Lars Poeschel <poeschel@lemonage.de>
2021-08-12 13:01:38 +0200

    InterfaceCodeGenerator: Add constructor to generated *Tuple class
    The generated Tuple classes were missing its constructor.

--- dbus-java-utils/src/main/java/org/freedesktop/dbus/utils/generator/InterfaceCodeGenerator.java
+++ dbus-java-utils/src/main/java/org/freedesktop/dbus/utils/generator/InterfaceCodeGenerator.java
@@ -434,6 +434,7 @@ public class InterfaceCodeGenerator {
         ClassConstructor cnstrct = new ClassConstructor();
         cnstrct.getArguments().addAll(_outputArgs);
 
+        info.getConstructors().add(cnstrct);
         info.getMembers().addAll(_outputArgs);
     	_additionalClasses.add(info);
 
