93e492b0574e3555bc102da478cd1a43493fe061
David M <maniac@case-of.org>
2021-09-15 15:35:11 +0200

    Added unit test to test connection to a newly created dbus daemon

--- dbus-java-tests/src/test/java/org/freedesktop/dbus/bin/EmbeddedDBusDaemonTest.java
+++ dbus-java-tests/src/test/java/org/freedesktop/dbus/bin/EmbeddedDBusDaemonTest.java
@@ -1,16 +1,67 @@
 package org.freedesktop.dbus.bin;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.io.IOException;
+import java.time.Duration;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.freedesktop.dbus.connections.BusAddress;
+import org.freedesktop.dbus.connections.impl.DBusConnection;
 import org.freedesktop.dbus.connections.transports.TransportFactory;
+import org.freedesktop.dbus.exceptions.DBusException;
+import org.freedesktop.dbus.test.AbstractBaseTest;
 import org.junit.jupiter.api.Test;
 
 /**
  *
  */
-public class EmbeddedDBusDaemonTest {
+public class EmbeddedDBusDaemonTest extends AbstractBaseTest {
+    /** Max wait time to wait for daemon to start. */
+    private static final long MAX_WAIT = Duration.ofSeconds(30).toMillis();
+
+    @Test
+    public void testStartAndConnectEmbeddedDBusDaemon() throws DBusException {
+        String protocolType = TransportFactory.getRegisteredBusTypes().get(0);
+        String newAddress = TransportFactory.createDynamicSession(protocolType, false);
+
+        BusAddress busAddress = new BusAddress(newAddress);
+        BusAddress listenBusAddress = new BusAddress(newAddress + ",listen=true");
+
+        logger.debug("Starting embedded bus on address {})", listenBusAddress.getRawAddress());
+        try (EmbeddedDBusDaemon daemon = new EmbeddedDBusDaemon(listenBusAddress)) {
+            daemon.startInBackground();
+            logger.debug("Started embedded bus on address {}", listenBusAddress.getRawAddress());
+
+            long sleepMs = 200;
+            long waited = 0;
+
+            while (!daemon.isRunning()) {
+                if (waited >= MAX_WAIT) {
+                    throw new RuntimeException("EmbeddedDbusDaemon not started in the specified time of " + MAX_WAIT + " ms");
+                }
+
+                try {
+                    Thread.sleep(sleepMs);
+                } catch (InterruptedException _ex) {
+                    break;
+                }
+
+                waited += sleepMs;
+                logger.debug("Waiting for embedded daemon to start: {} of {} ms waited", waited, MAX_WAIT);
+            }
+
+            // connect to started daemon process
+            logger.info("Connecting to embedded DBus {}", busAddress.getRawAddress());
+            try (DBusConnection conn = DBusConnection.getConnection(busAddress.getRawAddress())) {
+                logger.debug("Connected to embedded DBus {}", busAddress.getRawAddress());
+            } catch (Exception _ex) {
+                fail("Connection to EmbeddedDbusDaemon failed: " + _ex.getMessage());
+                logger.error("Error connecting to EmbeddedDbusDaemon", _ex);
+            }
+        } catch (IOException _ex1) {
+            fail("Failed to start EmbeddedDbusDaemon: " + _ex1.getMessage());
+            logger.error("Error starting EmbeddedDbusDaemon", _ex1);
+        }
+    }
 
     @Test
     public void test_start_stop() throws Exception {
