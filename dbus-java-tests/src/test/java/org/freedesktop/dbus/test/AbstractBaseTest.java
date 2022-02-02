22afe2a71fb69abd9154f179814dc4478727c3d5
David M <maniac@case-of.org>
2022-02-02 17:45:44 +0100

    Improved test to print current test method begin/end

--- dbus-java-tests/src/test/java/org/freedesktop/dbus/test/AbstractBaseTest.java
+++ dbus-java-tests/src/test/java/org/freedesktop/dbus/test/AbstractBaseTest.java
@@ -1,6 +1,12 @@
 package org.freedesktop.dbus.test;
 
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,4 +19,41 @@ import org.slf4j.LoggerFactory;
 public class AbstractBaseTest extends Assertions {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
+    /** Holds information about the current test. */
+    private TestInfo lastTestInfo;
+    
+    @BeforeEach
+    public final void setTestMethodName(TestInfo _testInfo) {
+        lastTestInfo = _testInfo;
+    }
+
+    protected final String getTestMethodName() {
+        if (lastTestInfo != null && lastTestInfo.getTestClass().isPresent()) {
+            return lastTestInfo.getTestClass().get().getName() + '.' + lastTestInfo.getTestMethod().get().getName();
+        }
+        return null;
+    }
+
+    protected final String getShortTestMethodName() {
+        Optional<Method> testMethod = lastTestInfo == null ? Optional.empty() : lastTestInfo.getTestMethod();
+        return testMethod.map(Method::getName).orElse(null);
+    }
+
+    @BeforeEach
+    public final void logTestBegin(TestInfo _testInfo) {
+        logTestBeginEnd("BGN", _testInfo);
+    }
+
+    @AfterEach
+    public final void logTestEnd(TestInfo _testInfo) {
+        logTestBeginEnd("END", _testInfo);
+    }
+
+    protected void logTestBeginEnd(String _prefix, TestInfo _testInfo) {
+        if (!_testInfo.getTestMethod().isPresent() || _testInfo.getDisplayName().startsWith(_testInfo.getTestMethod().get().getName())) {
+            logger.info(">>>>>>>>>> {} Test: {} <<<<<<<<<<", _prefix, _testInfo.getDisplayName());
+        } else {
+            logger.info(">>>>>>>>>> {} Test: {} ({}) <<<<<<<<<<", _prefix, _testInfo.getTestMethod().get().getName(), _testInfo.getDisplayName());
+        }
+    }
 }
