1ff92b4bcecbda356b0ee6a6f8878c1ce615a9d1
David M <maniac@case-of.org>
2023-08-16 11:49:10 +0200

    Issue #228: Improved example to show how to create proper list of structs

--- dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/systemd/PrintUserSessions.java
+++ dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/systemd/PrintUserSessions.java
@@ -1,11 +1,16 @@
 package com.github.hypfvieh.dbus.examples.systemd;
 
+import org.freedesktop.dbus.DBusPath;
+import org.freedesktop.dbus.Struct;
+import org.freedesktop.dbus.StructHelper;
+import org.freedesktop.dbus.annotations.Position;
 import org.freedesktop.dbus.connections.impl.DBusConnection;
 import org.freedesktop.dbus.connections.impl.DBusConnectionBuilder;
 import org.freedesktop.dbus.exceptions.DBusException;
 import org.freedesktop.dbus.interfaces.Properties;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 /**
@@ -31,18 +36,21 @@ import java.util.List;
  * suitable type.
  * </p>
  * <p>
- * The only way to deal with that is to stick to Object[] and use the values of it directly.
- * As the return type is defined as dbus type '{so}', we know that the struct is of type
- * String and DBusPath. This means: Object[0] = String, Object[1] = DBusPath.
+ * To deal with this you have two options:<br><br>
+ * 1) Stick to Object[] and use the values of it directly.<br>
+ *    As the return type is defined as dbus type '{so}', we know that the struct is of type<br>
+ *    String and DBusPath. This means: Object[0] = String, Object[1] = DBusPath.<br><br>
+ * 2) Use {@link StructHelper#convertToStructList(List, Class)} to create a proper List of structs.<br>
  * </p>
  *
  * @author hypfvieh
+ * 
  * @since 2023-02-27
  */
 public final class PrintUserSessions {
     private PrintUserSessions() {}
 
-    public static void main(String[] _args) throws IOException, DBusException {
+    public static void main(String[] _args) throws IOException, DBusException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         try (DBusConnection sessionConnection = DBusConnectionBuilder.forSystemBus().build()) {
 
             // fetch properties
@@ -51,11 +59,37 @@ public final class PrintUserSessions {
             // get the 'Sessions', which returns a complex type
             List<Object[]> sessions = properties.Get("org.freedesktop.login1.User", "Sessions");
 
-            // print all sessions
+            // Option 1: Use object array directly:
             for (Object[] us : sessions) {
                 System.out.println(us[0] + " --> " + us[1]);
+            }
+            
+            // Option 2: Use StructHelper and appropriate struct class
+            List<SessionStruct> convertToStruct = StructHelper.convertToStructList(sessions, SessionStruct.class);
+            for (SessionStruct us : convertToStruct) {
+                System.out.println(us.getUser() + " --> " + us.getDbusPath());
             }
         }
     }
 
+    public static class SessionStruct extends Struct {
+        @Position(0)
+        private final String user;
+        @Position(1)
+        private final DBusPath dbusPath;
+        
+        public SessionStruct(String _user, DBusPath _dbusPath) {
+            user = _user;
+            dbusPath = _dbusPath;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public DBusPath getDbusPath() {
+            return dbusPath;
+        }
+        
+    }
 }
---
+++ src/site/markdown/howto.md
@@ -0,0 +1,32 @@
+# HowTo
+
+
+Here are some references to example code to demonstrate how to...
+
+### Get Properties from DBus
+ * [PulseAudioDbus](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/pulseaudio/PulseAudioDbus.java) 
+ * [PrintUserSessions](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/systemd/PrintUserSessions.java)
+
+### Get array of struct DBus Properties
+ * [PrintUserSessions](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/systemd/PrintUserSessions.java)
+
+### Use structs
+ * [StructServer/StructClient](https://github.com/hypfvieh/dbus-java/tree/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/struct)
+ 
+### Get a remote interface
+ * [NetworkManagerExample](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/networkmanager/NetworkManagerExample.java)
+ * [NetworkManagerExample2](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/networkmanager/NetworkManagerExample2.java)
+ * [ControlVlcExample](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/mpris/ControlVlcExample.java)
+ 
+### Dealing with signals
+ * [PropertiesChangedSignalSample](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/signal/PropertiesChangedSignalSample.java)
+ 
+### Export an nested object on DBus
+ * [ExportNested](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/nested/ExportNested.java)
+ 
+### Use EmbeddedDBusDaemon
+ * [RunDaemon](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/daemon/RunDaemon.java)
+ 
+### Using Variant<?> with proper type
+ * [NetworkManagerExample3](https://github.com/hypfvieh/dbus-java/blob/master/dbus-java-examples/src/main/java/com/github/hypfvieh/dbus/examples/networkmanager/NetworkManagerExample3.java)
+ * See also: [Issue 74](https://github.com/hypfvieh/dbus-java/issues/74#issuecomment-1280768515)
